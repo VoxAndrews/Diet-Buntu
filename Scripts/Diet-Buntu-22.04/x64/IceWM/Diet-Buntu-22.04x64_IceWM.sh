1c3e0df2ce876bcd8c3c7e6fdf18377d5f5e6e9b
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2023-12-28 14:40:40 +0800

    Further Package Changes
    - Added extra packages for supporting extra features
    - Replaces GNOME Software with AppGrid for a lighter, more reliable (Hopefully) app installation experience (Testing will be conducted to make sure it works)

--- Scripts/Diet-Buntu-22.04/x64/IceWM/Diet-Buntu-22.04x64_IceWM.sh
+++ Scripts/Diet-Buntu-22.04/x64/IceWM/Diet-Buntu-22.04x64_IceWM.sh
@@ -259,13 +259,21 @@ begin_installation() {
 
     echo "Debug: Installing software and libraries from Ubuntu" >>/home/$the_user/debug.txt
 
-    local ubuntu_packages=(git build-essential libpam0g-dev libxcb1-dev xorg nano libgl1-mesa-dri lua5.3 vlc libgtk2.0-0 xterm pcmanfm pulseaudio pavucontrol gvfs-backends gvfs-fuse qtbase5-dev libqt5x11extras5-dev libqt5svg5-dev libhunspell-dev qttools5-dev-tools qview galculator cups printer-driver-gutenprint system-config-printer lxrandr clamav clamav-daemon libtext-csv-perl libjson-perl gnome-icon-theme cron libcommon-sense-perl libencode-perl libjson-xs-perl libtext-csv-xs-perl libtypes-serialiser-perl libcairo-gobject-perl libcairo-perl libextutils-depends-perl libglib-object-introspection-perl libglib-perl libgtk3-perl libfont-freetype-perl libxml-libxml-perl inotify-tools acpi lxappearance iputils-ping lightdm lightdm-gtk-greeter dbus connman connman-doc connman-gtk)
+    local ubuntu_packages=(git build-essential libpam0g-dev libxcb1-dev xorg nano libgl1-mesa-dri lua5.3 vlc libgtk2.0-0 xterm pcmanfm pulseaudio pavucontrol gvfs-backends gvfs-fuse qtbase5-dev libqt5x11extras5-dev libqt5svg5-dev libhunspell-dev qttools5-dev-tools qview galculator cups printer-driver-gutenprint system-config-printer lxrandr clamav clamav-daemon libtext-csv-perl libjson-perl gnome-icon-theme cron libcommon-sense-perl libencode-perl libjson-xs-perl libtext-csv-xs-perl libtypes-serialiser-perl libcairo-gobject-perl libcairo-perl libextutils-depends-perl libglib-object-introspection-perl libglib-perl libgtk3-perl libfont-freetype-perl libxml-libxml-perl inotify-tools acpi lxappearance iputils-ping lightdm lightdm-gtk-greeter dbus connman connman-doc connman-gtk libimlib2 libqt5printsupport5)
     install_packages "${ubuntu_packages[@]}"
 
     # Check the user's choice for the Utility Software Package
     if [ "$utility_option" == "1" ]; then
-        install_packages claws-mail gnupg libgpgme11 libetpan20 libldap-2.5-0 aspell aspell-en enchant-2 libenchant-2-2 libenchant-2-voikko bogofilter claws-mail-bogofilter spamassassin claws-mail-spamassassin gnome-software drawing
+        # Install the AppGrid repository
+        sudo add-apt-repository ppa:appgrid/stable
 
+        # Update the repositories
+        sudo apt update 
+        
+        # Install the Utility Software Packages
+        install_packages claws-mail gnupg libgpgme11 libetpan20 libldap-2.5-0 aspell aspell-en enchant-2 libenchant-2-2 libenchant-2-voikko bogofilter claws-mail-bogofilter spamassassin claws-mail-spamassassin appgrid drawing
+
+        # Download and Install FreeOffice
         download_and_install_deb "https://www.softmaker.net/down/softmaker-freeoffice-2021_1064-01_amd64.deb"
     fi
 
